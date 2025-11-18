f9e31625fa3b686c7f1cb4afa707d383b8b4cb27
c0a24025 <c0a240256e@edu.teu.ac.jp>
2025-11-18 16:46:41 +0900

    練習6:FPSを200に変更

--- pygame_image.py
+++ pygame_image.py
@@ -22,7 +22,7 @@ def main():
         screen.blit(kokaton_img, [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(1)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
